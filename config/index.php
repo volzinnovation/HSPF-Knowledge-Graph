d56f6bff442cc65ef6ed058bd32f66049e3707b8
Alessandro Jaeger <44026744+AlessandroJaeger@users.noreply.github.com>
2018-10-30 16:48:16 +0100

    Add files via upload

---
+++ config/index.php
@@ -0,0 +1,17 @@
+<?php require_once"config.php"; ?>
+
+<!DOCTYPE html>
+
+
+<html>
+    <head>
+        <meta charset="<?php $website_charset?>" />
+        <title>
+            <?php echo $website_title;?>
+        </title>
+    </head>
+    <body>
+        <?php include_once 'views/login.php'; ?>
+    </body>
+</html>
+
