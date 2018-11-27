65cca5bb73e146e66a9c5af1b2d0ff81fa9a3d55
raphaelvolz <rv@nogago.com>
2018-11-27 22:54:41 +0100

    Update UpdateOwnerId.java
    Cleaned up unnecessary comments and code

--- index/de.hspf.ai.kg/src/main/java/UpdateOwnerId.java
+++ index/de.hspf.ai.kg/src/main/java/UpdateOwnerId.java
@@ -26,18 +26,14 @@ import java.util.stream.Collectors;
 public class UpdateOwnerId {
     static String path = "C:\\temp\\nudel\\data\\index";
 
-    static double min_tfidf = 50000.0; // Filter items with term length adjusted TF IDF larger this value
-    static int min_df = 10; // Minimum Document Frequency
-    static String term, course;
-    static int tf, df;
-    static String field = "courseid";
+     static String term, course;
 
     /* Produce Pattern
      { "id" : "mod_folder-activity-873-solrfile1336477",
       "owneruserid" : { "set" : [ "-1" ] }
      }
     */
-    public static String toJSON(String docId, List<String> newOwners) {
+    private static String toJSON(String docId, List<String> newOwners) {
         if(newOwners == null || newOwners.size()==0 ) return null;
         String ret = "{ \"id\" : \"" + docId + "\",\"owneruserid\" : { \"set\" : [";
         for (String s : newOwners) {
@@ -50,10 +46,9 @@ public class UpdateOwnerId {
     }
 
     /**
-     * Run Update Owner Ids
+     * Run Update Owner Ids to produce JSON to upload manually into SolR admin interface
      *
      * @param args 0 Path
-     * @param args 1 Minimum Length-adjusted TFIDF
      */
     public static void main(String args[]) {
         MappingUtils mu = new MappingUtils();
@@ -62,7 +57,6 @@ public class UpdateOwnerId {
             // First argument is the path of the lucene index
             path = args[0];
         }
-        print("Courses: " + mu.getCourseIds().size());
         // Connecting to Lucene index at given path
         java.nio.file.Path p = Paths.get(path);
         try {
@@ -70,42 +64,11 @@ public class UpdateOwnerId {
             Directory dir = FSDirectory.open(p);
             DirectoryReader ir = DirectoryReader.open(dir);
             for (int j = 0; j < ir.maxDoc(); j++) {
-            /*
-            IndexSearcher searcher = new IndexSearcher(ir);
-            GermanAnalyzer analyzer = new org.apache.lucene.analysis.de.GermanAnalyzer();
-
-            // for (String course : mu.getCourseIds()) {
-            print("Course: " + course);
-            TopDocs docs = null;
-            // Search for term and accept 1 million results
-
-            // Query query = new QueryParser(field, analyzer).parse(course);
-            Query query = new FieldValueQuery(field);
-            docs = searcher.search(query, 1000000);
-            docs = ir.d
-            // Found documents for the search term
-            if (docs != null) {
-                ScoreDoc[] hits = docs.scoreDocs;
-                print(" Hits: " + hits.length);
-                if (hits.length > 0) {
-                    // Aggregate term frequencies by courses in map courses
-                    HashMap<String, Integer> courses = new HashMap<String, Integer>();
-                    // Aggregate term frequencies by course id
-                    // TODO Could make this more effective and push to Lucene using faceted search instead of plain search
-                    for (int j = 0; j < hits.length; j++) {
-                        int docId = hits[j].doc;
-                        */
-                //Document d = searcher.doc(docId);
                 Document d = ir.document(j);
-                // Count Occurences of search results per course
                 String id = d.get("id");
                 String course = d.get("courseid");
                 String json = toJSON(id, mu.getProfessorIds(course));
                 if(json != null) print(json + (j < (ir.maxDoc()-2) ? "," : ""));
-                // Print each search Result
-                // print((j + 1) + ": '" + d.get("title") + "' course #" + d.get("courseid"));
-
-
             }
             ir.close();
             dir.close();
